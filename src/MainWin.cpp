72688c27ac7176ad6030ba49527367e53b9fab4e
n3wtron <n3wtron@545ca29a-b1a4-11dd-a561-c1dc4c822220>
2008-11-14 23:21:49 +0000

    .exe mancante nell'esecuzione della compilazione
    git-svn-id: http://qdot.googlecode.com/svn/trunk@10 545ca29a-b1a4-11dd-a561-c1dc4c822220

--- src/MainWin.cpp
+++ src/MainWin.cpp
@@ -244,7 +244,7 @@ void MainWin::compile() {
         QString dest=actualEditor->getFileName().left(actualEditor->getFileName().lastIndexOf("."));
 
 #ifdef Q_WS_WIN
-        QString basicCmd=QString("%1\\%2")
+        QString basicCmd=QString("%1\\%2.exe")
                          .arg(setting.value("Application/graphvizBinaryDir").toString())
                          .arg(ui.cmbEngine->currentText());
         QStringList env = QProcess::systemEnvironment();
